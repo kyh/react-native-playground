df3d120b12b4f642b60d53d7abb291bfa381ddb9
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-02-28 21:13:15 -0800

    Add and style album covers

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,62 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-export default function AlbumDetail(props) {
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+    marginLeft: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
+
+export default function AlbumDetail({ album }) {
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerTextStyle,
+    thumbnailContainerStyle,
+    headerContentStyle,
+    imageStyle
+  } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{ uri: image }}
+        />
       </CardSection>
     </Card>
   );
