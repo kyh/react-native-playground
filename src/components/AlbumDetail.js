360e2b3e64d03ed6b006046dece9aec10eb86f43
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-02-28 21:45:08 -0800

    Add reusable buttons

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const styles = {
   headerContentStyle: {
@@ -29,7 +30,7 @@ const styles = {
 };
 
 export default function AlbumDetail({ album }) {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const {
     thumbnailStyle,
     headerTextStyle,
@@ -52,11 +53,18 @@ export default function AlbumDetail({ album }) {
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onClick={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
