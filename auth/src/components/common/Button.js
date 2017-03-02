1017c78451486aae8f8e58d5bc49e74aff88fcff
Kaiyu Hsu <im.kaiyu@gmail.com>
2017-03-01 23:15:40 -0800

    Copy components over

---
+++ auth/src/components/common/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#007aff',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
+
+export function Button({ onClick, children }) {
+  const { buttonStyle, textStyle } = styles;
+  return (
+    <TouchableOpacity onPress={onClick} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+}
---
+++ auth/src/components/common/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export function Header({ title }) {
+  const { viewStyle, textStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{title}</Text>
+    </View>
+  );
+}
